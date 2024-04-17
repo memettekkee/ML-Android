fbe24ca1946b1ccb964486d960b2e9f48eb95774
Muhammad <muhammadahjahh@gmail.com>
2024-04-17 17:05:37 +0800

    barcode scan fix

--- BarcodeScanner/app/build.gradle.kts
+++ BarcodeScanner/app/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
+    implementation("androidx.camera:camera-mlkit-vision:1.4.0-alpha02")
+
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
